a85618da47e700fc56b3dd1a8a3cab61effb251a
Aj Wemple <aj.wemple@umontana.edu>
2023-11-27 03:46:07 +0000

    Cleaned up flaskDB.py

--- DB Webpage/flaskDB.py
+++ DB Webpage/flaskDB.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
 import mysql.connector, os, json
 
-# Isn't this also a security vulnerability? I wasn't sure how else to specify the path, I tried using a relative path but that didn't work. 
-secrets = os.path.abspath('/home/aj/secrets/secrets.json')
-with open(secrets, 'r') as secretFile:
+
+with open('/home/aj/secrets/secrets.json', 'r') as secretFile:
     creds = json.load(secretFile)['mysqlcredentials']
 
 app = Flask(__name__)
@@ -18,4 +17,4 @@ def showCustomer():
     connection.close()
     return render_template('customer.html', customers=myresult)
 if __name__ == '__main__':
-    app.run(port=8001, debug=True, host="0.0.0.0")
+    app.run(port=8000, debug=True, host="0.0.0.0")
